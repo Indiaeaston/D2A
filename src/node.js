e86a789cafcb424abca50bec4c50ec956f808b0e
leemorgan03 <leemorgan.cs@gmail.com>
2024-11-13 04:22:28 -0500

    Google Cloud Speech-to-Text API is up and running for testing. To set up the server and API, clone repository and read the Set Up Instructions. Can add more audios or start playing around with translating for the future.

--- src/node.js
+++ src/node.js
@@ -8,5 +8,32 @@ app.get('/', function(req, res){
 app.listen(3000, () => {
   console.log("listening on http://localhost:3000");
 })
+const fs = require('fs');
+const { SpeechClient } = require('@google-cloud/speech');
 
+const client = new SpeechClient();
 
+async function transcribeFile(filePath) {
+  const file = fs.readFileSync(filePath);
+  const audioBytes = file.toString('base64');
+
+  const [response] = await client.recognize({
+    config: {
+      encoding: 'MP3',
+      sampleRateHertz: 16000, // Adjust if needed
+      languageCode: 'en-US',
+    },
+    audio: {
+      content: audioBytes,
+    },
+  });
+
+  const transcription = response.results
+    .map(result => result.alternatives[0].transcript)
+    .join('\n');
+
+  console.log(`Transcription: ${transcription}`);
+}
+
+const filePath = "C:\\Users\\leemo\\OneDrive\\Documents\\Senior Capstone\\D2A\\src\\Identity_Theft_Is_Not_A_Joke.mp3";
+transcribeFile(filePath);
